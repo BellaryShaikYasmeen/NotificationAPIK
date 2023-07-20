ee3588fa6117498c9843cd6bfb3859b176195d49
Yasmeen Shaik <bellaryshaikyasmeen@gmail.com>
2023-07-20 19:39:13 +0530

    Push Notification Code refactor

--- app/src/main/java/com/example/notificationapik/MainActivity.kt
+++ app/src/main/java/com/example/notificationapik/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.example.notificationapik
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.os.Build
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RemoteViews
+
 
 class MainActivity : AppCompatActivity() {
 
